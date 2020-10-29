6cc5140d943fa2bb7b091cf8329e8e7034819b40
KunitcynSemyon <43928719+KunitcynSemyon@users.noreply.github.com>
2020-10-29 23:54:21 +0400

    Update settings.py

--- gamenews/settings.py
+++ gamenews/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9$18n%3cq46^z!&_4e7jqar2*jxxh(n5#o$slnwzl=18d9j-dd'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
